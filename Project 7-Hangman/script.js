5927a27f7049d00ab966f63eaa2c2f9c497ce5cd
AniModi <98632268+AniModi@users.noreply.github.com>
2022-08-25 23:55:07 +0530

    Completed body of hangman

--- Project 7-Hangman/script.js
+++ Project 7-Hangman/script.js
@@ -11,18 +11,37 @@ function choice(chosen){
     document.querySelector(".game-page").style.display='grid';
     let canvas = document.querySelector('canvas');
     let ctx = canvas.getContext('2d');
-    ctx.lineWidth=0.5;
+    //Drawing board
     ctx.beginPath();
-    ctx.rect(20,50,10,100);
+    ctx.rect(20,50,1,100);
     ctx.stroke();
-    ctx.beginPath()
-    ctx.rect(15,55,130,2);
+    ctx.beginPath();
+    ctx.rect(15,55,130,1);
     ctx.stroke();
-    ctx.beginPath()
-    ctx.rect(125,58,5,10);
+    ctx.beginPath();
+    ctx.rect(125,58,1,10);
     ctx.stroke();
-    ctx.beginPath()
-    ctx.arc(127,77,7,0,2*Math.PI);
+    //Drawing head
+    ctx.beginPath();
+    ctx.arc(125,78,7,0,2*Math.PI);
+    ctx.stroke();
+    //Drawing torso
+    ctx.beginPath();
+    ctx.rect(125,87,1,20);
+    ctx.stroke();
+    //Drawing hands
+    ctx.beginPath();
+    ctx.rect(100,90,50,1);
+    ctx.stroke();
+    //Drawing Leg 1
+    ctx.rotate(Math.PI/6);
+    ctx.beginPath();
+    ctx.rect(160,31,1,15);
+    ctx.stroke();
+    //Drawing Leg 2
+    ctx.rotate(-Math.PI/3);
+    ctx.beginPath();
+    ctx.rect(56,157,1,15);
     ctx.stroke();
     if(ch==="Country"){
         document.querySelector(".country").style.display='grid';
