b1e106d21b028d100ef317462d5d7c072363fe4b
AniModi <98632268+AniModi@users.noreply.github.com>
2022-08-26 16:04:11 +0530

    midway commit

--- Project 7-Hangman/script.js
+++ Project 7-Hangman/script.js
@@ -1,9 +1,88 @@
 function play(){
-    // alert("HI")
     document.querySelector(".intro-page").style.display='none';
     document.querySelector(".choice-menu").style.display='grid';
-    // console.log(intro);
 }
+
+
+function randomQuestion(ch){
+    if(ch==="Country"){
+        let i = Math.floor(Math.random()*(countries.length-1));
+        return setBoard(i,ch);
+    }
+    else if(ch==="Animals"){
+        let i = Math.floor(Math.random()*(animals.length-1));
+        console.log(animals[i])
+        return setBoard(i,ch);
+    }
+    else if(ch==="Presidents"){
+        let i = Math.floor(Math.random()*(Presidents.length-1));
+        return setBoard(i,ch);
+    }
+    else{
+        let i = Math.floor(Math.random()*3);
+        i==0?randomQuestion("Country"):i==1?randomQuestion("Animals"):randomQuestion("Presidents");
+    }
+}
+
+
+function setHint(h){
+    document.querySelector(".hint").textContent=h;
+}
+
+
+function setBoard(ind,ch){
+    let answer = {};
+    let question;
+    if(ch==="Country"){
+        question=countries[ind];
+    }
+    else if(ch==="Animals"){
+        question=animals[ind];
+    }
+    else{
+        question=Presidents[ind];
+    }
+    let q = question.name;
+    let h = question.hint;
+    let words = q.split(/[ ,]+/).filter(Boolean);
+    let i =0;
+    let f = -1;
+    let letters = document.querySelectorAll(".letters");
+    for(let w=0;w<words.length;w++){
+        if(10-i<words[w].length){
+            f=w;
+            for(;i<10;i++)
+            letters[i].style.border='none';
+            break;
+        }
+        for(let c=0;c<words[w].length;c++){
+            i++;
+        }
+        letters[i].style.border='none';
+    }
+    if(f!==-1){
+        for(let w=f;w<words.length;w++){
+            for(let c=0;c<words[w].length;c++){
+                answer[i]=w;
+                i++;
+            }
+            letters[i].style.border='none';
+        }
+    }
+    for(;i<20;i++)
+    letters[i].style.border='none';
+    setHint(h);
+    return [q,answer];
+}
+
+
+
+function letterClick(btn){
+    return btn.textContent;
+}
+
+
+
 
 function choice(chosen){
     let ch = chosen.textContent;
@@ -43,7 +122,18 @@ function choice(chosen){
     ctx.beginPath();
     ctx.rect(56,157,1,15);
     ctx.stroke();
+    document.querySelector(".game").style.display='grid';
     if(ch==="Country"){
-        document.querySelector(".country").style.display='grid';
+        let arr = randomQuestion(ch);
     }
+    else if(ch==="Animals"){
+        let arr = randomQuestion(ch);
+    }
+    else if(ch==="Presidents"){
+        let arr = randomQuestion(ch);
+    }
+    else if(ch==="Random from above"){
+        let arr = randomQuestion(ch);
+    }
+
 }
