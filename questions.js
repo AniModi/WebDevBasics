1e05669e9c9282a6d42ec8c819b4de2183011701
AniModi <98632268+AniModi@users.noreply.github.com>
2022-08-26 22:30:41 +0530

    fixed a glitch in random choice

---
+++ questions.js
@@ -0,0 +1,27 @@
+const animals = [
+    {name:"dog",
+    hint:"loyal"},
+    {name:"cat",hint:"loves milk"},
+    {name:"cow",hint:"farmers animal"},
+    {name:"hen",hint:"farmers animal"},
+    {name:"husky",hint:"dramatic animals"},
+    {name:"horse",hint:"fast runners"},
+    {name:"elephants",hint:"giant animals"},
+]
+const countries = [
+    {name:'India',hint:"Known for rich heritage"},
+    {name:'United States',hint:"One of the super powers"},
+    {name:'China',hint:"The third largest country"},
+    {name:'Canada',hint:"The second largest country"},
+    {name:'Russia',hint:"Give me Vodka"},
+    {name:'Australia',hint:"I am also a continent"},
+    {name:'England',hint:"Tumhe dugna lagaan dena hoga!!"},
+]
+
+
+const Presidents = [
+    {name:"Abraham Lincoln",hint:"The American President during civil war"},
+    {name:"Pratibha Patil",hint:"India's first women president"},
+    {name:"Ronald Reagan",hint:"USA actor turned president also famous for his Soviet Jokes"},
+    {name:"Mikhail Gorbachev",hint:"Last president of Soviet Union"},
+]
